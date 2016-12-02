a937a7654fa9859919c79fc99f904fa12c568dab
kihong08 <kihong08@gmail.com>
2016-12-02 17:22:34 +0900

    class and constructor

--- stageCtrl.cpp
+++ stageCtrl.cpp
@@ -1,19 +1,61 @@
 #include <iostream>
 using namespace std;
 
-int forward(int stagePos) {
-    cout << "stage moved forward" << '\n';
-    stagePos ++;
+class stage {
+private:
+    int serial;
+    int initPos;
+public:
+    //void setInfo(int _initPos, int _serial);
+    stage(int _serial, int _initPos, int _jogStep);
+    int forward();
+    int backward();
+    void status();
+    int currentPos;
+    int jogStep;
+};
 
-    return stagePos;
+stage::stage(int _serial, int _initPos, int _jogStep) {
+    serial = _serial;
+    initPos = _initPos;
+    currentPos = initPos;
+    jogStep = _jogStep;
+}
+
+int stage::forward() {
+    cout << "stage moved forward" << '\n';
+    currentPos = currentPos + jogStep;
+
+    return currentPos;
+}
+
+int stage::backward() {
+    cout << "stage moved backward" << '\n';
+    currentPos = currentPos - jogStep;
+
+    return currentPos;
+}
+
+void stage::status() {
+    cout << "stage number: " << serial << '\n';
+    cout << "jog step size: " << jogStep << '\n';
+    cout << "initial position: " << initPos << '\n';
+    cout << "current position: " << currentPos << '\n';
 }
 
 int main() {
+    int serial = 2323, initPos;
     int command = 0;
-    int stagePos = 0;
+    int jogStep;
 
     cout << "Enter initial stage position:" << '\n';
-    cin >> stagePos;
+    cin >> initPos;
+
+    cout << "Enter jog step size:" << '\n';
+    cin >> jogStep;
+
+    stage stg(serial, initPos, jogStep);
+
 
     while (1) {
         cout << "Select command - 1: moveFoward, 2: moveBackward, 3: currentPosition" << '\n';
@@ -21,12 +63,11 @@ int main() {
         cin >> command;
 
         if (command == 1) {
-            stagePos = forward(stagePos);
+            stg.forward();
         } else if (command == 2) {
-            cout << "stage moved backward" << '\n';
-            stagePos --;
+            stg.backward();
         } else if (command == 3) {
-            cout << "Current stageg position is: " << stagePos << '\n';
+            stg.status();
         } else if (command == 4) {
             break;
         }
